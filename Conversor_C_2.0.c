d52d2cded71ce1182734b305981ffe0fb453c1fc
Lucca Bessa <LuccaBessa@users.noreply.github.com>
2019-06-02 20:38:37 -0300

    Mudanças muito loucas

--- Conversor_C_2.0.c
+++ Conversor_C_2.0.c
@@ -6,54 +6,14 @@
 char valorBase[Max];
 int valorDecimal;
 
-void converter(int opcao)
+void inverterVetor(char *Vetor, int cont)
 {
-    int value = 0, base = 0;
-    switch (opcao)
-    {
-    case 1:
-        printf("Digite um numero de decimal para converter: ");
-        scanf("%d", &value);
-        printf("Digite a base para qual deseja converter (2, 8 e 16): ");
-        scanf("%d", &base);
-        printf("O valor convertido eh: %s", decimaltodasbases(value, base));
-        break;
-
-    case 2:
-        printf("Digite um numero em binario para converter para qualquer base: ");
-        scanf("%s", valorBase);
-        printf("Digite a base para qual deseja converter (8, 10 e 16): ");
-        scanf("%d", &base);
-        printf("O valor convertido eh: %s", decimaltodasbases(todasbasesdecimal(valorBase, 2), base));
-        break;
-
-    case 3:
-        printf("Digite um numero em octal converter para qualquer base: ");
-        scanf("%s", valorBase);
-        printf("Digite a base para qual deseja converter (2, 10 e 16): ");
-        scanf("%d", &base);
-        printf("O valor convertido eh: %s", decimaltodasbases(todasbasesdecimal(valorBase, 8), base));
-        break;
-
-    case 4:
-        printf("Digite um numero em octal converter para qualquer base: ");
-        scanf("%s", valorBase);
-        printf("Digite a base para qual deseja converter (2, 8 e 10): ");
-        scanf("%d", &base);
-        printf("O valor convertido eh: %s", decimaltodasbases(todasbasesdecimal(valorBase, 16), base));
-        break;
-
-    default:
-        break;
-    }
-}
-
-void vetorinvertido(char *Vetor, int cont){
     int j = 0, k = 0;
     char aux_troca[Max];
     j = 0;
-    k = cont-1;
-    while (j < k) {
+    k = cont - 1;
+    while (j < k)
+    {
         aux_troca[j] = Vetor[j];
         Vetor[j] = Vetor[k];
         Vetor[k] = aux_troca[j];
@@ -62,7 +22,7 @@ void vetorinvertido(char *Vetor, int cont){
     }
 }
 
-char* decimaltodasbases(int Valordecimal, int Base){
+char* decimalTodasBases(int Valordecimal, int Base){
     int i = 0, valor = 0;
     while (Valordecimal) {
         valor = Valordecimal%Base;
@@ -76,14 +36,14 @@ char* decimaltodasbases(int Valordecimal, int Base){
         i++;
     }
     valorBase[i] = '\0';
-    vetorinvertido(valorBase, i);
+    inverterVetor(valorBase, i);
     return valorBase;
 }
 
-int todasbasesdecimal(char *N, int Base){
+int todasBasesDecimal(char *N, int Base){
     int tamanho = 0,i;
     tamanho = (int)strlen(N);
-    vetorinvertido(N, (int)strlen(N));
+    inverterVetor(N, (int)strlen(N));
     for (i = 0; i < tamanho; i++) {
         if (N[i] >= 48 && N[i] <= 59) {
             valorDecimal += pow(Base, i)*(N[i]-48);
@@ -96,12 +56,71 @@ int todasbasesdecimal(char *N, int Base){
 }
 
 int main(){
-    int option= 0;
+    int option= 0, value = 0, base = 0;
 
     printf("Conversor de bases\n\n");
     printf("Digite um numero para escolher uma opcao.\n1-Decimal.\n2-Binario.\n3-Octal.\n4-Hexadecimal.\nOpcao: ");
     scanf("%d", &option);
-    converter(option);
+
+    switch (option){
+        case 1:
+            printf("Digite um numero de decimal para converter: ");
+            scanf("%d", &value);
+            printf("Digite a base para qual deseja converter (2, 8 e 16): ");
+            scanf("%d", &base);
+            if (base == 2|| base == 8 || base == 16){
+                printf("O valor convertido eh: %s", decimalTodasBases(value, base));
+                break;
+            }else{
+                printf("Base não encontarda ou não permitida!\n");
+                break;
+            }
+            
+            case 2:
+                printf("Digite um numero em binario para converter para qualquer base: ");
+                scanf("%s", valorBase);
+                printf("Digite a base para qual deseja converter (8, 10 e 16): ");
+                scanf("%d", &base);
+                if (base == 8 || base == 10 || base == 16){
+                    printf("O valor convertido eh: %s", decimalTodasBases(todasBasesDecimal(valorBase, 2), base));
+                    break;
+                }
+                else{
+                    printf("Base não encontarda ou não permitida!\n");
+                    break;
+                }
+
+            case 3:
+                printf("Digite um numero em octal converter para qualquer base: ");
+                scanf("%s", valorBase);
+                printf("Digite a base para qual deseja converter (2, 10 e 16): ");
+                scanf("%d", &base);
+                if (base == 2 || base == 8 || base == 16){
+                    printf("O valor convertido eh: %s", decimalTodasBases(todasBasesDecimal(valorBase, 8), base));
+                    break;
+                }
+                else{
+                    printf("Base não encontarda ou não permitida!\n");
+                    break;
+                }
+
+            case 4:
+                printf("Digite um numero em octal converter para qualquer base: ");
+                scanf("%s", valorBase);
+                printf("Digite a base para qual deseja converter (2, 8 e 10): ");
+                scanf("%d", &base);
+                if (base == 2 || base == 8 || base == 10){
+                    printf("O valor convertido eh: %s", decimalTodasBases(todasBasesDecimal(valorBase, 16), base));
+                    break;
+                }else{
+                    printf("Base não encontarda ou não permitida!\n");
+                    break;
+                }
+
+        default:
+            printf("Base não encontarda!\n");
+            break;
+    }
 
     return 0;
 }
