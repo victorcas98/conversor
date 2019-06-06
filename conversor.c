e72ce72dee1ccef97070115d1197da32c100ae74
Lucca Bessa <LuccaBessa@users.noreply.github.com>
2019-06-05 23:07:44 -0300

    Removendo comentarios

--- conversor.c
+++ conversor.c
@@ -4,46 +4,36 @@
 
 #define tamPadrao 255
 
-// Recebe como parâmetro um número decimal (num_dec), e a base (base) cujo número será convertido.
 void decimalParaBase(int num, int base) {
    char resto[17] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}, numeroConvertido[tamPadrao];
    int i = 0;
-   // Este loop realiza as operações de divisão. Os restos são guardados no vetor 'num_conv'
    while(num > 0) {
        numeroConvertido[i] = resto[num % base];
        num /= base;
        i++;
    }
-   // Exclui o caractere especial '\0' do final da string
+
    i -= 1;
-   // Utilizado para identificar a saída do programa
+
    switch(base) {
        case 2 : printf("BINÁŔIO: "); break;
        case 8 : printf("OCTAL: "); break;
        case 16 : printf("HEXADECIMAL: "); break;
    }
     
-   // Inverte a string contida no vetor numeroConvertido
    for(i; i >= 0; i--){
        printf("%c", numeroConvertido[i]);
    } 
    printf("\n");
 }
 
-// Esta função permite converter um número na base 2, 8 ou 16 para a base decimal. 
-// Recebe como parâmetro uma string (representação do número), e um número inteiro que 
-// determina em qual base está o número do primeiro parâmetro.
 void baseParaDecimal(char* num, int base) {
     int i = 0, exp = 0, tam = 0, dec = 0, num1 = 0;
     
-    // Este loop determina o tamanho da string
     while(num[tam] != '\0') {
         tam++;
     }
     
-    // Neste loop, a string é percorrida de trás para frente. Cada caractere é convertido
-    // em um número inteiro, multiplicado pela base elevada ao expoente (0..Comprimento da string) 
-    // e somado ao valor da variável 'dec', que recebe as somas sucessivas. 
     for(exp, i=(tam-1); exp < tam; exp++, i--) {
         if(base == 16) {
             switch(num[i]) {
@@ -90,7 +80,6 @@ void baseParaDecimal(char* num, int base) {
         else
             num1 = (int)num[i] - (int)'0';
 
-        // Aqui avalia se o usuário digitou um número válido ou não!
         if(num1 > (base-1) || num1 < 0) {
             printf("Numero invalido!\n");
             exit(0);
@@ -98,7 +87,6 @@ void baseParaDecimal(char* num, int base) {
         dec += num1 * pow(base, exp);
     }
 
-    // O número convertido para decimal é exibido
     printf("DECIMAL: %d\n", dec);
 }
 
